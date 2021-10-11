2c68bab32e047b60679954ba0c6b319949bb47cc
nessy-lee <74070605+nessy-lee@users.noreply.github.com>
2021-10-10 23:15:55 -0700

    more edits to fancify

--- assets/scripts/fancifymytext.js
+++ assets/scripts/fancifymytext.js
@@ -1,35 +1,35 @@
-const btn = document.querySelector('button');
-const btn2 = document.getElementById("button 2");
-const moo = document.getElementById("Moo");
+//alert
+function alertz() {
+   alert("You fancy!"); 
+} 
 
-
-btn.onclick = function() {
-    //alert("Hello, world!");
-    document.getElementById("mybox").style.fontSize = "24px";
-}
-
-btn2.onclick = function() {
-    alert("Radio button");
-}
-
-moo.onclick = function() {
-    document.getElementById("mybox").style.textTransform = "uppercase";
-    var str = document.getElementById("mybox").value;
-    var parts = str.split(" ");
-    str = parts.join("-Moo "); 
-    document.getElementById("mybox").value = str;
-
-}
-
-
-function changeBox() {
-    if(document.getElementById("mybox").style.fontWeight == "normal"){
-        document.getElementById("mybox").style.fontWeight = "bold";
-        document.getElementById("mybox").style.textDecoration = "underline";
-        document.getElementById("mybox").style.color = "blue";
-    } else {
-        document.getElementById("mybox").style.fontWeight = "normal";
-        document.getElementById("mybox").style.textDecoration = "none";
-        document.getElementById("mybox").style.color = "black";
-    }
-}
+//makes font bigger
+function biggie() {
+    var text = document.getElementById("growing");
+    text.style.fontSize = "24pt";
+ }
+ 
+ //makes font bolded
+ function bolding() {
+    var text = document.getElementById("growing");
+    text.style.fontWeight = "bold";
+    text.style.color= "blue";
+    text.style.textDecoration= "underline";
+ }
+ 
+ //makes the font unbolded
+ function unbolding() {
+    var text = document.getElementById("growing");
+    text.style.fontSize = "initial";
+    text.style.fontWeight = "normal";
+    text.style.color = "black";
+    text.style.textDecoration= "none";
+ }
+ 
+ //makes the font uppercase and adds Moo suffix
+ function mooing() {
+    var suffix = '-Moo',
+    txt = document.getElementById('growing');
+    txt.style.textTransform="uppercase";
+    txt.value = txt.value.split('.').join('') + suffix;
+ }
